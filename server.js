6589bb086657bd30adfbe2da91bf12c7c7c7da3b
singcl <you@example.com1276837689@qq.com>
2017-12-20 14:10:20 +0800

    React项目基础搭建

---
+++ server.js
@@ -0,0 +1,20 @@
+const Koa = require('koa')
+const send = require('koa-send')
+const Router = require('koa-router')
+
+const app = new Koa()
+const router = new Router()
+
+router.get('/', async (ctx) => {
+  await send(ctx, 'demo/index.html')
+})
+
+router.get('/app.js', async (ctx) => {
+  await send(ctx, 'build/app.js')
+})
+
+app.use(router.routes())
+
+app.listen(3000, () => {
+  console.log('server running on http://localhost:3000')
+})
